427b3fb7b40049e127cc27fd60e39b64ede56cf7
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-10 22:46:11 -0300

    Modificaciones

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+//Programacion asincrona
+
+Stopwatch sw = Stopwatch.StartNew();
+
+var task = new Task(() =>
+{
+    Stopwatch crono = new Stopwatch();
+    crono.Start();
+    Thread.Sleep(1000);
+    crono.Stop();
+    //codigo, Task, etc
+    //int num = 0;
+    /*for (int i = 0; i <= 1000; i++) {
+        num += i;
+    }
+    crono.Stop();
+    */
+    Console.WriteLine($"Task 1: {crono.Elapsed}");
+});
+
+var task2 = new Task(() =>
+{
+    Stopwatch crono = new Stopwatch();
+    crono.Start();
+    Thread.Sleep(1000);
+    crono.Stop();
+    //codigo, Task, etc
+    //int num = 0;
+    /*for (int i = 0; i <= 1000; i++) {
+        num += i;
+    }
+    crono.Stop();
+    */
+    Console.WriteLine($"Task 2:{crono.Elapsed}");
+});
+
+var task3 = new Task(() =>
+{
+    Stopwatch crono = new Stopwatch();
+    crono.Start();
+    Thread.Sleep(1000);
+    crono.Stop();
+    //codigo, Task, etc
+    //int num = 0;
+    /*for (int i = 0; i <= 1000; i++) {
+        num += i;
+    }
+    crono.Stop();
+    */
+    Console.WriteLine($"Task 3: {crono.Elapsed}");
+});
+
+task.Start();
+task2.Start();
+task3.Start();
+
+await task;
+await task2;
+await task3;
+sw.Stop();
+Console.WriteLine($"Todo Main:{sw.Elapsed}");
